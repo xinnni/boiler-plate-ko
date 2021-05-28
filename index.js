1acae2c502aff3b86e67cbdc8a0d181f4096ece9
양희진 <labyrinth.0901@gmail.com>
2021-05-28 22:21:43 +0900

    fixed: heejin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5500;
 const { User } = require('./models/User');
 const config = require('./config/key')
 // const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -11,6 +12,7 @@ mongoose.connect(config.mongoURI, {
   .catch(error => console.log(error))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser());
 
   // app.get('/', (req, res) => {
   //   res.send('하이룽삼')
@@ -30,7 +32,37 @@ app.post('/register', (req, res) => {
   })
   // 몽고에서 주는 메서드: 정보들이 유저 모델에 저장이 된다.
 })
+app.post('/login', (req, res) => {
 
+  // 요청된 이메일을 데이터 베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+  user.comparePassword(req.body.password, (err, isMatch) => {
+    if(!isMatch)
+      return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+   user.generateToken((err, user) => {
+      if(err) return res.status(400).send(err);
+
+      // 토큰을 저장한다. 어디에?? 쿠키, 로컬 스토리지, 등등 
+      res.cookie("x_auth", user.token)
+      .status(200)
+      .json({ loginSuccess: true, userId: user._Id })
+   })
+  })
+  })
+})
+
+// 요청한 이메일이 있다면 비밀번호가 같은지 확인해야 한다.
+
+// 비밀번호가 맞다면 토큰(??)을 생성해야 한다. 
+
+// 토큰이란..? 
 
 
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+// const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+
+
 
 // 모델은 무엇인가
 // 스키마를 감싸 주는 역할을 한다.
@@ -42,6 +48,51 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const User = mongoose.model('user', userSchema)
+userSchema.pre('save', function( next ) {
+
+    let user = this;
+     // 비밀번호를 암호화 시킨다. 
+    // 암호화 시키기 위해 ..
+
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if(err) return next(err)
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next()
+    }
+})
+// 유저 모델에 유저 정보를 저장하기 전에, 무엇을 한다는 뜻.
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+    // 비밀번호를 비교할 때 플레인 패스워드가 있다면 데이터 베이스에 있는 암호화된 비밀번호가 같은지 체크를 해야 하는데,
+    // 들어오는 비밀번호도 암호화를 해야 한다;; 
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return callback(err),
+        callback(null, isMatch)
+    })
+}
+
+ userSchema.methods.generateToken = function(callback) {
+     // 웹토큰을 이용해서 토큰을 생성해야 한다.
+     let user = this;
+     let token = jwt.sign(user._id.toString(), 'secretToken')
+
+    // user._id + 'secretToken' = token
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return callback(err)
+        callback(null, user)
+    })
+ }
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
